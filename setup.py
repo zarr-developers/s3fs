c5a4863d11e69200630d9c937e6e49dedaf5108f
Martin Durant <martin.durant@utoronto.ca>
2018-03-14 13:25:29 -0400

    v0.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 setup(name='s3fs',
-      version='0.1.3',
+      version='0.1.4',
       description='Convenient Filesystem interface over S3',
       url='http://github.com/dask/s3fs/',
       maintainer='Martin Durant',
