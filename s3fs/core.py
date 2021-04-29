88fa59b651eb28d2fa9f84fbd405cb81442432ed
Batuhan Taskaya <batuhanosmantaskaya@gmail.com>
2021-04-29 12:59:23 +0300

    info: process the raw path on version aware mode

--- s3fs/core.py
+++ s3fs/core.py
@@ -923,6 +923,9 @@ class S3FileSystem(AsyncFileSystem):
             raise FileNotFoundError(path)
     
     async def _version_aware_info(self, path):
+        path = self._strip_protocol(path)
+        bucket, key, _ = self.split_path(path)
+
         out = await self._call_s3(
             "head_object",
             self.kwargs,
